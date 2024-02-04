394a2f750a1b502493a48da98ff362e96766fbf4
Sir-bastian <sebamadziwanzira@gmail.com>
2024-02-04 04:50:27 -0800

    Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,38 +1,52 @@
 #!/usr/bin/python3
 """
-A fabric script based on 1-pack_web_static.py that distributes an archive to
-web servers using the function do_deploy
+a Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers, using the function do_deploy
 """
-import os
-from os import getenv, environ
-from os.path import exists
-from fabric.api import *
 
+
+from fabric.api import *
+from os.path import exists
+from os import getenv, environ
 
 env.hosts = ['54.236.48.22', '54.85.37.215']
+env.user = 'ubuntu'
+env.key_filename = '/home/root/.ssh/id_rsa'
 
 
-@task
 def do_deploy(archive_path):
-    if not os.path.exists(archive_path):
-        print(f"The file at path {archive_path} doesn't exist.")
+    """Deploys the web static to the server"""
+    if not exists(archive_path):
+        print("path does not exist\n")
         return False
 
-    remote_dir = '/data/web_web_static/releases'
-    archive_filename = os.path.basename(archive_path)
-    destination_dir = f"{remote_dir}/{os.path.splitext(archive_filename)[0]}"
+    try:
+        archive_name = archive_path.split('/')[-1]
+        file_name = archive_name.split('.')[0]
+        sym_link = "/data/web_static/current"
+        release_version = f"/data/web_static/releases/{file_name}/"
 
-    for server in env.hosts:
-        with Connection(server) as conn:
-            remote_archive_path = f"/tmp/{archive_filename}"
-            conn.put(archive_path, remote_archive_path)
+        # deploying locally
+        run_locally = getenv("run_locally", None)
+        if run_locally is None:
+            print(f"Deploying new_version from {archive_path}")
+            local(f"sudo mkdir -p {release_version}")
+            local(f"sudo tar -xzf {archive_path} \
+-C {release_version} --strip-components=1")
+            local(f"sudo rm -f {sym_link}")
+            local(f"sudo ln -s {release_version} {sym_link}")
+            environ['run_locally'] = "True"
+            print("Deployed locally\n")
 
-            conn.run(f"sudo mkdir -p {destination_dir}")
-            conn.run(f"sudo tar -xzf {remote_archive_path} -C
-                     {destinaation_dir} --strip-components=1")
-            conn.run(f"sudo rm {remote_archive_path}")
-
-            conn.run(f"sudo rm -rf /data/web_static/current")
-            conn.run(f"sudo ln -s {destination_dir} /data/web_static/current")
-
-    return True
+        put(archive_path, f"/tmp/{archive_name}")
+        run(f"mkdir -p {release_version}")
+        run(f"tar -xzf /tmp/{archive_name} \
+-C {release_version} --strip-components=1")
+        run(f"rm /tmp/{archive_name}")
+        run(f"rm -f {sym_link}")
+        run(f"ln -s {release_version} {sym_link}")
+        print(f"New Version Deployed --> {release_version}")
+        return True
+    except Exception as e:
+        print(f"Failed to Deploy New Version --> {release_version}\n{str(e)}")
+        return False
